1000a76bf50c8ed279d4734f2eda119e815c2e8c
jgoldsmith83 <mapexmelee@gmail.com>
2015-10-26 20:41:19 -0400

    more cleaning up after git fucked it all up. merge conflicts my ass.

--- Testing/test.sh
+++ Testing/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-<<<<<<< HEAD
+
 function countdown() {
 	for ((i=$1; i>0; i--)); do
 	case $1 in
@@ -22,18 +22,3 @@ function countdown() {
 }
 
 countdown 10
-=======
-readarray tester < test.txt
-
-for i in ${tester[@]}; do
-	echo ${i}
-done
-
-IFS=$'\n' array2=($(< test.txt))
-
-echo ""
-
-for i in ${tester[@]}; do
-	echo ${i}
-done
->>>>>>> 1e9d06b9c3dbc15c3e54dd03c61b0127c0aab5f6

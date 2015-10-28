23cd835edcb6a9644d830873c83f596e15cbd7a7
Justin Goldsmith <justingoldsmith@my.tridenttech.edu>
2015-10-28 11:34:09 -0400

    Changes to runme.sh - added validation for port numbers rather than any string - this is to protect x11vnc from corruption by adding 'abcd' as a port number in x11vnc.conf

--- runme.sh
+++ runme.sh
@@ -37,14 +37,14 @@
 
 if [ ! $UID -eq 0 ]; then
 	echo ""
-	echo "**************************************************************"
-	echo "*                                                            *"
-	echo "*  This script must be run as root. Include 'sudo' like so:  *"
-	echo "*                sudo ./runme.sh [port]                      *"
-	echo "*                                                            *"
-	echo "*             Please re-run the script as root               *"
-	echo "*                                                            *"
-	echo "**************************************************************"
+	echo "      **************************************************************"
+	echo "      *                                                            *"
+	echo "      *  This script must be run as root. Include 'sudo' like so:  *"
+	echo "      *                sudo ./runme.sh [port]                      *"
+	echo "      *                                                            *"
+	echo "      *             Please re-run the script as root               *"
+	echo "      *                                                            *"
+	echo "      **************************************************************"
 	echo ""
         echo ""
 	echo ""
@@ -54,21 +54,46 @@ fi
 
 if [ -z $1 ]; then
 	echo ""
-	echo "************************************************************"
-	echo "*                                                          *"
-	echo "*  You must provide a port number as an argument like so:  *"
-	echo "*               sudo ./runme.sh [port]                     *"
-	echo "*                                                          *"
-	echo "* Please re-run the command with a port number to be used  *"
-	echo "* for vnc remote desktop.                                  *"
-	echo "*                                                          *"
-	echo "************************************************************"
+	echo "      ************************************************************"
+	echo "      *                                                          *"
+	echo "      *  You must provide a port number as an argument like so:  *"
+	echo "      *               sudo ./runme.sh [port]                     *"
+	echo "      *                                                          *"
+	echo "      * Please re-run the command with a port number to be used  *"
+	echo "      * for vnc remote desktop.                                  *"
+	echo "      *                                                          *"
+	echo "      ************************************************************"
 	echo ""
-        echo ""
+    echo ""
+	echo ""
+
+	exit 3
+fi
+
+if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+	echo ""
+	echo ""
+	echo "      ************************************************************"
+	echo "      *                                                          *"
+	echo "      *  Port number must be a number. Try again like so:        *"
+	echo "      *               sudo ./runme.sh 5900                       *"
+	echo "      *                                                          *"
+	echo "      *        Ports should correspond to the machine:           *"
+	echo "      *                    5900 = Station 1                      *"
+	echo "      *                    5901 = Station 2                      *"
+	echo "      *                    5902 = Station 3                      *"
+	echo "      *                      and so on...                        *"
+	echo "      *                                                          *"
+	echo "      *        Please re-run the script with a port number       *"
+	echo "      *                                                          *"
+	echo "      ************************************************************"
+	echo ""
+    echo ""
 	echo ""
 
 	exit 2
 fi
+
 
 #sudo apt-get install python-tk
 
